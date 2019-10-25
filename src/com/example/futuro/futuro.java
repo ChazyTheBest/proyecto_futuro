84e980e81dc3dd65f9e798b85ae0d97e24f782f2
Gabriel Blanca Montes <how2hack@hotmail.es>
2019-10-25 15:23:29 +0200

    versión inicial

---
+++ src/com/example/futuro/futuro.java
@@ -0,0 +1,33 @@
+package com.example.futuro;
+
+import java.io.PrintStream;
+
+public class futuro
+{
+    public static String veredicto(String valor1, String valor2)
+    {
+        String msg;
+
+        if (valor1.equals(valor2))
+        {
+            msg = "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+
+        else if (Integer.parseInt(valor1) < Integer.parseInt(valor2))
+        {
+            msg = "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+
+        else
+        {
+            msg = "Ideal. Trabajas los contenidos en casa.";
+        }
+
+        return msg;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.printf("Estudiante %s:\n- Módulos registrados: %s\n- Horas: %s", args[0], args[1], veredicto(args[2], "1"));
+    }
+}
